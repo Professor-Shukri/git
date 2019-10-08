7d7aff53117937f08f8a28a9783600aa595f85b6
Shukri Abotteen <s_abotteen@fhsu.edu>
2019-10-07 19:56:21 -0500

    Update class1.java

--- git/src/git/class1.java
+++ git/src/git/class1.java
@@ -1,13 +1,5 @@
 package git;
 
 public class class1 {
-<<<<<<< HEAD
-//change from desktop shukri
-<<<<<<< HEAD
-  //change from server shukri
-=======
->>>>>>> parent of d02eb2a... Update class1.java
-=======
->>>>>>> parent of d8aa249... Update class1.java
 
 }
