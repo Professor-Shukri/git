e27fdfdaaec3c038895f510216c833431b21b27b
Shukri Abotteen <s_abotteen@fhsu.edu>
2019-10-07 19:55:18 -0500

    Revert "Revert "Update class1.java""
    This reverts commit 6e2616b3d9a05a85c148f91d65a21232ddfd6b52.

--- git/src/git/class1.java
+++ git/src/git/class1.java
@@ -4,9 +4,12 @@ public class class1 {
 <<<<<<< HEAD
 //change from desktop shukri
 <<<<<<< HEAD
+<<<<<<< HEAD
   //change from server shukri
 =======
 >>>>>>> parent of d02eb2a... Update class1.java
+=======
+>>>>>>> parent of d8aa249... Update class1.java
 =======
 >>>>>>> parent of d8aa249... Update class1.java
 
